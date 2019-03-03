c0d2595565b3c4b57c72b62a7f294ce955e311ad
Joshua SA <jshiesa@gmail.com>
2019-03-02 23:06:45 -0500

    artists in a text file

--- python/doYouKnow.py
+++ python/doYouKnow.py
@@ -4,8 +4,13 @@ import sys
 found = False
 
 input = sys.argv[1]
-a_file = open('artists.pk', 'rb')
-artists = pickle.load(a_file)
+# a_file = open('artists.pk', 'rb')
+# artists = pickle.load(a_file)
+# a_file.close()
+a_file = open('artists.txt', 'r')
+artists = list()
+for line in a_file:
+    artists.append(line[:-1])
 a_file.close()
 
 if input.lower() in artists:
