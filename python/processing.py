dd1bb6287b9ebb67c398c7dc89a6bd933181769d
Joshua SA <jshiesa@gmail.com>
2019-03-03 02:13:51 -0500

    stuff

--- python/processing.py
+++ python/processing.py
@@ -1,10 +1,11 @@
 import language_check
 tool = language_check.LanguageTool('en-US')
 
-def fix_grammar(str):
-    matches = tool.check(str)
+
+def fix_grammar(s):
+    matches = tool.check(s)
     print(len(matches))
-    return language_check.correct(str, matches)
+    return language_check.correct(s, matches)
 
 
 text = u'A sentence with a error in the Hitchhiker’s Guide tot he Galaxy'
