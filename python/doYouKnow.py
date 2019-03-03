8a3e1f61edc4093137a19b5884098767afc4fcde
Timothy Han <whapplez@gmail.com>
2019-03-02 21:25:17 -0500

    doyouknow

--- python/doYouKnow.py
+++ python/doYouKnow.py
@@ -8,13 +8,13 @@ a_file = open('artists.pk', 'rb')
 artists = pickle.load(a_file)
 
 if input in artists:
-    print("do")
+    sys.stdout.write("do")
 
 else:
     for i in range(len(artists)):
         if input in artists[i].lower():
-            print("do")
+            sys.stdout.write("do")
             found = True
             break
     if not found:
-        print("don't")
+        sys.stdout.write("don't")
