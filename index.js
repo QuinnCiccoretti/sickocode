8696309107bbbd704eab9cf2b314083c1c64022e
Timothy Han <whapplez@gmail.com>
2019-03-02 21:20:57 -0500

    doyouknow

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ assistantApp.intent('doyouknow', conv => {
     var artist = conv.parameters.artist;
     pythonFile = path.join(__dirname, 'python', 'doYouKnow.py');
     var process = spawn(python_exe, [pythonFile, artist]);
+    console.log(process.stdout);
     conv.ask("I " + process.stdout + "know " + artist);
 });
 
