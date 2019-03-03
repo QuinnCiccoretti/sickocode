84d3eb2c02ddcb3f1b1cbd4f28e96e58228f4a86
Timothy Han <whapplez@gmail.com>
2019-03-02 22:00:50 -0500

    lower

--- python/doYouKnow.py
+++ python/doYouKnow.py
@@ -8,12 +8,12 @@ a_file = open('artists.pk', 'rb')
 artists = pickle.load(a_file)
 a_file.close()
 
-if input in artists:
+if input.lower() in artists:
     sys.stdout.write("do ")
 
 else:
     for i in range(len(artists)):
-        if input in artists[i].lower():
+        if input.lower() in artists[i].lower():
             sys.stdout.write("do ")
             found = True
     if not found:
