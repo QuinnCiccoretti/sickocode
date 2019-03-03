44020a13b3431de762a0b411f590296b98756948
Timothy Han <whapplez@gmail.com>
2019-03-03 01:02:50 -0500

    raps2.0

--- python/py_script_01.py
+++ python/py_script_01.py
@@ -7,12 +7,13 @@ f = open("./rap.json", "r")
 rapList = json.load(f)
 
 theRap = ""
+rapper = rapper.lower().replace(" ", "-")
 
-if rapper.lower().replace(" ", "-") not in rapList["raps"]:
+if rapper not in rapList["raps"]:
     theRap += "I can't find that rapper. Random rapper chosen. "
     rapper = "kendrick-lamar"
 
 theRap += random.choice(rapList["raps"][rapper])
 
-print(rapList["raps"][rapper])
+# print(rapList["raps"][rapper])
 sys.stdout.write(theRap)
--- python/py_script_02.py
+++ python/py_script_02.py
@@ -7,8 +7,9 @@ f = open("./rap.json", "r")
 rapList = json.load(f)
 
 theRap = ""
+rapper = rapper.lower().replace(" ", "-")
 
-if rapper.lower().replace(" ", "-") not in rapList["raps"]:
+if rapper not in rapList["raps"]:
     theRap += "I can't find that rapper. Random rapper chosen. "
     rapper = "kendrick-lamar"
 
