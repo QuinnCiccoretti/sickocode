1633b6d419733dd5bb2fe04e3a6ffd64d87c69ee
Timothy Han <whapplez@gmail.com>
2019-03-02 23:11:24 -0500

    buffertostirng

--- python/doYouKnow.py
+++ python/doYouKnow.py
@@ -1,12 +1,8 @@
-import pickle
 import sys
 
 found = False
 
 input = sys.argv[1]
-# a_file = open('artists.pk', 'rb')
-# artists = pickle.load(a_file)
-# a_file.close()
 a_file = open('artists.txt', 'r')
 artists = list()
 for line in a_file:
