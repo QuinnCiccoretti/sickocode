fa85428ba585a877e4a0662f6fe0594ebeba3124
Timothy Han <whapplez@gmail.com>
2019-03-02 23:16:37 -0500

    yeet

--- python/doYouKnow.py
+++ python/doYouKnow.py
@@ -8,14 +8,14 @@ artists = list()
 for line in a_file:
     artists.append(line[:-1])
 a_file.close()
-
-if input.lower() in artists:
-    sys.stdout.write("do ")
-
-else:
-    for i in range(len(artists)):
-        if input.lower() in artists[i].lower():
-            sys.stdout.write("do ")
-            found = True
-    if not found:
-        sys.stdout.write("don't ")
+sys.stdout.write("God damn it. ")
+# if input.lower() in artists:
+#     sys.stdout.write("do ")
+#
+# else:
+#     for i in range(len(artists)):
+#         if input.lower() in artists[i].lower():
+#             sys.stdout.write("do ")
+#             found = True
+#     if not found:
+#         sys.stdout.write("don't ")
