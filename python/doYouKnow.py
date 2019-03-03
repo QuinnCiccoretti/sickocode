b062fda5452b57306a6d1c9878c9bcf62b1c2994
Timothy Han <whapplez@gmail.com>
2019-03-02 22:30:23 -0500

    requirements

--- python/doYouKnow.py
+++ python/doYouKnow.py
@@ -8,13 +8,14 @@ a_file = open('artists.pk', 'rb')
 artists = pickle.load(a_file)
 a_file.close()
 
-if input.lower() in artists:
-    sys.stdout.write("do ")
-
-else:
-    for i in range(len(artists)):
-        if input.lower() in artists[i].lower():
-            sys.stdout.write("do ")
-            found = True
-    if not found:
-        sys.stdout.write("don't ")
+sys.stdout.write(artists[1]);
+# if input.lower() in artists:
+#     sys.stdout.write("do ")
+#
+# else:
+#     for i in range(len(artists)):
+#         if input.lower() in artists[i].lower():
+#             sys.stdout.write("do ")
+#             found = True
+#     if not found:
+#         sys.stdout.write("don't ")
