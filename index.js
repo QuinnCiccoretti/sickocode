5f360433169bfeb8a13e41f5e10146927753aa60
Timothy Han <whapplez@gmail.com>
2019-03-02 22:10:14 -0500

    requirements

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ assistantApp.intent('doyouknow', conv => {
     var artist = conv.parameters.artist;
     pythonFile = path.join(__dirname, 'python', 'doYouKnow.py');
     var process = spawn(python_exe, [pythonFile, artist]);
-    console.log(process.stdout[1]);
+    console.log(process.stdout);
     conv.ask("I " + process.stdout + "know " + artist);
 });
 
