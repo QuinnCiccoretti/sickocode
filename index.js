355da2082cd3943ee538e1f2f32e9e670fe98607
QuinnCiccoretti <quinnc423@gmail.com>
2019-03-03 04:26:35 -0500

    updated weights

--- index.js
+++ index.js
@@ -63,13 +63,13 @@ assistantApp.intent('raplike', conv => {
     var e1 = '</prosody></media><media fadeOutDur = "5.0s" end = "rap.end+5.0s"><audio src = "';
     var e2 = '"/></media></par></speak>';
     var rando = Math.random();
-    if(rando < 0){
+    if(rando < .33){
         conv.close(b1+ "3s"+ b2 + process.stdout + e1 + "https://jay-z.herokuapp.com/audio/taste.mp3" + e2);
     }
-    else if (rando < 0){
+    else if (rando < .66){
         conv.close(b1 + "10.5s"+b2 + process.stdout + e1 + "https://jay-z.herokuapp.com/audio/zeze.mp3"+e2);
     }
-    else if (rando < 1){
+    else{
         conv.close(b1 + "8s"+b2 + process.stdout + e1 + "https://jay-z.herokuapp.com/audio/humble.mp3"+e2);
     }
 });
