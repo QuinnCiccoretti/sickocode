b1b59d2cd52c0419b8b88dc970271e7943e335c4
Timothy Han <whapplez@gmail.com>
2019-03-02 22:50:47 -0500

    buffertostirng

--- python/doYouKnow.py
+++ python/doYouKnow.py
@@ -8,14 +8,13 @@ a_file = open('artists.pk', 'rb')
 artists = pickle.load(a_file)
 a_file.close()
 
-sys.stdout.write("bufferr I hackat soylent");
-# if input.lower() in artists:
-#     sys.stdout.write("do ")
-#
-# else:
-#     for i in range(len(artists)):
-#         if input.lower() in artists[i].lower():
-#             sys.stdout.write("do ")
-#             found = True
-#     if not found:
-#         sys.stdout.write("don't ")
+if input.lower() in artists:
+    sys.stdout.write("do ")
+
+else:
+    for i in range(len(artists)):
+        if input.lower() in artists[i].lower():
+            sys.stdout.write("do ")
+            found = True
+    if not found:
+        sys.stdout.write("don't ")
