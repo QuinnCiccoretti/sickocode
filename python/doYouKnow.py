948c1f7b5d3a5adc3d5f795532e55d4a6e29d740
Timothy Han <whapplez@gmail.com>
2019-03-02 23:47:35 -0500

    pickle

--- python/doYouKnow.py
+++ python/doYouKnow.py
@@ -4,18 +4,19 @@ found = False
 input = sys.argv[1]
 
 a_file = open("./python/artists.pk", "rb")
-artists = pickle.load(a_file)
-# for line in a_file:
-#     artists.append(line[:-1])
-a_file.close()
-
-if input.lower() in artists:
-    sys.stdout.write("do ")
-
-else:
-    for i in range(len(artists)):
-        if input.lower() in artists[i].lower():
-            sys.stdout.write("do ")
-            found = True
-    if not found:
-        sys.stdout.write("don't ")
+sys.stdout.write("do ")
+# artists = pickle.load(a_file)
+# # for line in a_file:
+# #     artists.append(line[:-1])
+# a_file.close()
+#
+# if input.lower() in artists:
+#     sys.stdout.write("do ")
+#
+# else:
+#     for i in range(len(artists)):
+#         if input.lower() in artists[i].lower():
+#             sys.stdout.write("do ")
+#             found = True
+#     if not found:
+#         sys.stdout.write("don't ")
