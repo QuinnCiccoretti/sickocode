a6377ad6f07c1e1ac299943103c397014ebf6114
Timothy Han <whapplez@gmail.com>
2019-03-02 23:46:04 -0500

    pickle

--- python/doYouKnow.py
+++ python/doYouKnow.py
@@ -1,19 +1,14 @@
 import sys
 
 found = False
-dbg = ""
 input = sys.argv[1]
-# sys.stdout.write(__dirname);
-a_file = open("./python/artists.txt", "r")
-# sys.stdout.write("God damn it. 4")
-artists = list()
-# sys.stdout.write("God damn it.3 ")
-for line in a_file:
-    # sys.stdout.write("God damn it. 2")
-    artists.append(line[:-1])
-# sys.stdout.write("God damn it.1 ")
+
+a_file = open("./python/artists.pk", "rb")
+artists = pickle.load(a_file)
+# for line in a_file:
+#     artists.append(line[:-1])
 a_file.close()
-# sys.stdout.write(""+dbg)
+
 if input.lower() in artists:
     sys.stdout.write("do ")
 
