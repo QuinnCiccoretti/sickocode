d9c2af4bb001f887ad30028449d40ae569155cf9
Timothy Han <whapplez@gmail.com>
2019-03-02 23:22:08 -0500

    gdi2

--- python/doYouKnow.py
+++ python/doYouKnow.py
@@ -1,19 +1,18 @@
 import sys
 
 found = False
-
+dbg = ""
 input = sys.argv[1]
-sys.stdout.write("God damn it. 5")
 a_file = open('artists.txt', 'r')
-sys.stdout.write("God damn it. 4")
+dbg+="God damn it. 4"
 artists = list()
-sys.stdout.write("God damn it.3 ")
+dbg+="God damn it.3 "
 for line in a_file:
-    sys.stdout.write("God damn it. 2")
+    dbg+="God damn it. 2"
     artists.append(line[:-1])
-sys.stdout.write("God damn it.1 ")
+dbg+="God damn it.1 "
 a_file.close()
-sys.stdout.write("God damn it. ")
+sys.stdout.write(""+dbg)
 # if input.lower() in artists:
 #     sys.stdout.write("do ")
 #
