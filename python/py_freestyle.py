67e0d7498d2555ed0fd374f333023d9d9f0a75f2
whapplez <whapplez@gmail.com>
2019-04-06 16:19:05 -0400

    regex censor

--- python/py_freestyle.py
+++ python/py_freestyle.py
@@ -2,19 +2,27 @@ import sys
 import json
 import random
 import string
+import re
 # read input buffer
 f = open("./rap.json", "r")
 rapList = json.load(f)
 rapper = random.choice(list(rapList["raps"].keys()))
 def censor(rap):
-    badwords = "motherfuck motherfucking motherfuckin' motherfucker f*****' f*** f****** fuck shit shits bitch bitches dick penis vagina pussy cunt nigga nigger niggas niggers".split()
-    i = 0
-    rapList = rap.split()
-    while i < len(rapList):
-        if rapList[i] in badwords:
+    badwords = "motherfuck motherfucking motherfuckin' fuckin' motherfucker f*****' f*** f****** fuck shit shits bitch bitches dick penis vagina pussy cunt nigga nigger niggas niggers".split()
+    rapList = rap.split();
+    for i in range(rapList):
+        # if [m.start() for m in re.finditer('fuck|shit|nigg|dick|penis|vagina|pussy|cunt', word)]:
+        if re.search('[a-z]*(fuck|shit|nigg|dick|penis|vagina|pussy|cunt)[a-z]*', rapList[i]) is not None:
             rapList[i] = "<say-as interpret-as='expletive'>censor</say-as>"
-        i += 1
+
     return " ".join(rapList)
+
+    # rapList = rap.split()
+    # while i < len(rapList):
+    #     if rapList[i] in badwords:
+    #         rapList[i] = "<say-as interpret-as='expletive'>censor</say-as>"
+    #     i += 1
+    # return " ".join(rapList)
 
 theRap = ""
 rapper = rapper.lower().replace(" ", "-")
